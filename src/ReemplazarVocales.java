62da8b3b05c979cd92fe17f38e39f761b885a9ce
Rubén Saiz <rsaizhdam1@gmail.com>
2022-05-18 20:11:12 +0200

    Primer commit

---
+++ src/ReemplazarVocales.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class ReemplazarVocales {
+	
+	/**
+	 * Reemplaza las vocales por numeros
+	 */
+	public static void main(String[] args) {
+		ReemplazarVocales rv = new ReemplazarVocales();
+		System.out.println(rv.reemplazarVocalesPorNumeros("EntornosDeDesarrollo"));
+	}
+    public String reemplazarVocalesPorNumeros(String texto) {
+		StringBuilder cifrado = new StringBuilder();
+		
+		
+		for (int i = 0; i < texto.length(); i++) {
+			if (Character.toUpperCase(texto.charAt(i)) == 'A') {
+				cifrado.append('4');
+			} else if (Character.toUpperCase(texto.charAt(i)) == 'E') {
+				cifrado.append('3');
+			} else if (Character.toUpperCase(texto.charAt(i)) == 'I') {
+				cifrado.append('1');
+			} else if (Character.toUpperCase(texto.charAt(i)) == 'O') {
+				cifrado.append('0');
+			} else if (Character.toUpperCase(texto.charAt(i)) == 'U') {
+				cifrado.append('7');
+			} else {
+				cifrado.append(texto.charAt(i));
+			}
+		}
+		return cifrado.toString();
+	}
+
+
+}
---
+++ test/ReemplazarVocalesTest.java
@@ -0,0 +1,59 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReemplazarVocalesTest {
+    private ReemplazarVocales reemplazar;
+    private String resul;
+    @BeforeEach
+    void setUp() {
+        reemplazar =  new ReemplazarVocales();
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void test1() {
+        resul = reemplazar.reemplazarVocalesPorNumeros("");
+        assertEquals(resul, "");
+    }
+    @Test
+    void test2() {
+        resul = reemplazar.reemplazarVocalesPorNumeros("a");
+        assertEquals("4", resul);
+    }
+    @Test
+    void test3() {
+        resul = reemplazar.reemplazarVocalesPorNumeros("e");
+        assertEquals("3", resul);
+    }
+    @Test
+    void test4() {
+        resul = reemplazar.reemplazarVocalesPorNumeros("i");
+        assertEquals("1", resul);
+    }
+    @Test
+    void test5() {
+        resul = reemplazar.reemplazarVocalesPorNumeros("o");
+        assertEquals("0", resul);
+    }
+    @Test
+    void test6() {
+        resul = reemplazar.reemplazarVocalesPorNumeros("u");
+        assertEquals("7", resul);
+    }
+    @Test
+    void test7() {
+        resul = reemplazar.reemplazarVocalesPorNumeros("g");
+        assertEquals("g", resul);
+    }
+    @Test
+    void test8() {
+        resul = reemplazar.reemplazarVocalesPorNumeros("aa");
+        assertEquals("44", resul);
+    }
+}
